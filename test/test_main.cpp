7faf9cb5f83846a259e8181a5c8012c6586ebc5d
Dawid Meleszczuk <dawid.meleszczuk@gmal.com>
2024-10-26 01:34:07 +0200

    pain of missing ()

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,19 +4,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //test 1
 
 int dumb_func()
@@ -41,37 +28,39 @@ EepromVariable eeprom_test2(test_manager);
 void reset_eeprom_variable()
 {
     eeprom_test1.change_eeprom_config_value(0.0);
+    delay(10);
     eeprom_test2.change_eeprom_config_value(0.0);
+    delay(10);
 }
 
 void change_eeprom_to_2137()
 {
     eeprom_test1.change_eeprom_config_value(2137.0);
+    delay(10);
     eeprom_test2.change_eeprom_config_value(2137.0);
+    delay(10);
 }
 
+float retrieve_eeprom_value()
+{
+    float temp;
+    temp = eeprom_test1.retrieve_config_values_from_eeprom();
+    Serial.println(temp);
+    return temp;
+}
 
 
 
 //test 2  FOR SOME REASON CODE WORK FINE BUT TEST DONT WORK RIGHT
 
-// float retrieve_eeprom_value_after_reset()
-// {
-//    return eeprom_test1.retrieve_config_values_from_eeprom();
-// }
 
-
-// void test_retrieve_eeprom_value_after_reset()
-// {
-//     TEST_ASSERT_EQUAL(0, retrieve_eeprom_value_after_reset);
-// }
+void test_retrieve_eeprom_value_after_reset()
+{
+    TEST_ASSERT_EQUAL(0.0, retrieve_eeprom_value());
+}
 
 //test 3
 
-float retrieve_eeprom_value()
-{
-    return eeprom_test1.retrieve_config_values_from_eeprom();
-}
 
 
 void test_retrieve_eeprom_value()
@@ -108,12 +97,15 @@ UNITY_BEGIN();
 delay(10);
 RUN_TEST(test_dumb_func);
 
-// delay(10);
-// reset_eeprom_variable();
-// Serial.print("EEPROM VALUE");
-// Serial.println(eeprom_test1.retrieve_config_values_from_eeprom());
-// delay(10);
-// RUN_TEST(test_retrieve_eeprom_value_after_reset);
+
+delay(10);
+Serial.println(eeprom_test1.retrieve_config_values_from_eeprom());
+reset_eeprom_variable();
+Serial.println(eeprom_test1.retrieve_config_values_from_eeprom());
+delay(10);
+Serial.println(eeprom_test1.retrieve_config_values_from_eeprom());
+RUN_TEST(test_retrieve_eeprom_value_after_reset);
+Serial.println(eeprom_test1.retrieve_config_values_from_eeprom());
 
 delay(10);
 change_eeprom_to_2137();
